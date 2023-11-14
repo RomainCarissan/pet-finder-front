979c77b5ffd13a88e5e0261fe0128da376fcd30e
romain.carissan2@gmail.com <romain.carissan2@gmail.com>
2023-11-14 10:34:19 +0100

    fixed typo

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar/NavBar.jsx";
+import Navbar from "../components/NavBar/NavBar.jsx";
 function Layout() {
   return (
     <>
